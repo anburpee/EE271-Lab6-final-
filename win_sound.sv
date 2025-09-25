4384cf01204f67004fb7a3378b6945ae3dc30df2
anburpee <ayaburpee@gmail.com>
2025-09-25 14:28:03 -0700

    removed theft_flagger, simple, sin_soud

--- win_sound.sv
+++
@@ -1,9 +0,0 @@
-//module win_sound(HEX, clk, reset, win, loss);
-//	output logic [6:0] HEX;
-//	input logic clk, reset, win, loss;
-//	
-//	enum {neither, success, failure} ps, ns;
-//	
-//	always_ff @(posedge clk) begin
-//		if (reset)
-//			
